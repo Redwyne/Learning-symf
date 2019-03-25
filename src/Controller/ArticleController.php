fc5a11475304e2e74a0b2922e42ea17ae0edf4b5
Kamel Michel <michel.kamel@ymail.com>
2019-03-25 22:05:33 +0100

    comprit le systeme de routage

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,12 +2,31 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController {
 
+
+  /**
+  * @Route("/")
+  */
+
   public function homepage()
+
   {
-      return new Response ('My first page');
+      return new Response("blaabla");
+
+  }
+
+  /**
+  * @Route("/news/{slug}")
+  */
+
+  public function show($slug) {
+    return new Response (sprintf(
+      "voila l'entrée: %s"
+    ,$slug));
+
   }
 }
